3dba48e36809756c379d1bdb39ad18cf408b987c
Matt McKegg <matt@wetsand.co.nz>
2017-12-11 14:16:09 +1300

    push latest code from npm module (can't find repo)

--- src/spellchecker.h
+++ src/spellchecker.h
@@ -3,17 +3,47 @@
 
 #include <string>
 #include <vector>
+#include <stdint.h>
 
 namespace spellchecker {
 
-// Initializes everything.
-void Init(const std::string& dirname);
+struct MisspelledRange {
+  size_t start;
+  size_t end;
+};
 
-// Returns true if the word is misspelled.
-bool IsMisspelled(const std::string& word);
+class SpellcheckerImplementation {
+public:
+  virtual bool SetDictionary(const std::string& language) = 0;
+  virtual bool SetDictionaryToContents(const unsigned char* data, size_t length) = 0;
+  
+  virtual std::vector<std::string> GetAvailableDictionaries(const std::string& path) = 0;
 
-// Returns an array containing possible corrections for the word.
-std::vector<std::string> GetCorrectionsForMisspelling(const std::string& word);
+  // Returns an array containing possible corrections for the word.
+  virtual std::vector<std::string> GetCorrectionsForMisspelling(const std::string& word) = 0;
+
+  // Returns true if the word is misspelled.
+  virtual bool IsMisspelled(const std::string& word) = 0;
+
+  virtual std::vector<MisspelledRange> CheckSpelling(const uint16_t *text, size_t length) = 0;
+
+  // Adds a new word to the dictionary.
+  // NB: When using Hunspell, this will not modify the .dic file; custom words must be added each
+  // time the spellchecker is created. Use a custom dictionary file.
+  virtual void Add(const std::string& word) = 0;
+  
+  // Removes a word from the custom dictionary added by Add.
+  // NB: When using Hunspell, this will not modify the .dic file; custom words must be added each
+  // time the spellchecker is created. Use a custom dictionary file.
+  virtual void Remove(const std::string& word) = 0;
+
+  virtual ~SpellcheckerImplementation() {}
+};
+
+class SpellcheckerFactory {
+public:
+  static SpellcheckerImplementation* CreateSpellchecker();
+};
 
 }  // namespace spellchecker
 
